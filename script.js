d24a5d15a04c9688935bb1441fc93f224647b7b1
Dev.Mohamed <102235955+MohamedAbobakr12@users.noreply.github.com>
2024-07-24 01:52:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const preloader = document.querySelector("[data-preloader]");
+
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        if (preloader) {
+            preloader.classList.add("remove");
+        }
+    }, 2000);
+});
+
+/**
+ * add event on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+    for(let i = 0, len = elements.length; i < len; i++){
+        elements[i].addEventListener(eventType, callback);
+    }
+}
+
+/**
+ * Navbar toggler for mobile
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNav = function() {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.add("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNav);
+
+
+/**
+ *  Header
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function() {
+    header.classList[window.scrollY > 100 ? "add" : "remove"]("active");
+});
